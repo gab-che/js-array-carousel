b8973a243bf08ae65240873117dc012c8e807082
gab-che <gabriellachessa1@gmail.com>
2022-11-04 19:48:19 +0100

    imgs now change on click

--- js/script.js
+++ js/script.js
@@ -6,11 +6,31 @@ const imgList = [
     "img/05.webp"
 ];
 
-const imgContainer = document.querySelector(".img_container");
+let currentImgIndex = 0;
+const lastIndex = imgList.length - 1;
+
+const imgCarousel = document.querySelector(".img_carousel");
+imgCarousel.src = imgList[currentImgIndex];
 
 const btnUp = document.getElementById("button_up");
 const btnDown = document.getElementById("button_down");
 
-let currentImgIndex = 0;
+btnUp.addEventListener("click", function(){
+    currentImgIndex--;
 
-imgContainer.src = imgList [currentImgIndex];
+    if (currentImgIndex < 0){
+        currentImgIndex = lastIndex;
+    }
+
+    imgCarousel.src = imgList[currentImgIndex];
+})
+
+btnDown.addEventListener("click", function(){
+    currentImgIndex++;
+
+    if (currentImgIndex > lastIndex){
+        currentImgIndex = 0;
+    }
+
+    imgCarousel.src = imgList[currentImgIndex];
+})
