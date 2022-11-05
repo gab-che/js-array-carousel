5e14573352079cac9982a1ab24e9af944c4badfd
gab-che <gabriellachessa1@gmail.com>
2022-11-05 13:53:05 +0100

    js now adds img dynamically

--- js/script.js
+++ js/script.js
@@ -6,31 +6,34 @@ const imgList = [
     "img/05.webp"
 ];
 
-let currentImgIndex = 0;
+let currentIndex = 0;
 const lastIndex = imgList.length - 1;
 
-const imgCarousel = document.querySelector(".img_carousel");
-imgCarousel.src = imgList[currentImgIndex];
+const imgContainer = document.querySelector(".img_container");
+const newImg = document.createElement("img");
+newImg.classList.add("img_carousel");
+newImg.src = imgList[currentIndex];
+imgContainer.append(newImg);
 
 const btnUp = document.getElementById("button_up");
 const btnDown = document.getElementById("button_down");
 
 btnUp.addEventListener("click", function(){
-    currentImgIndex--;
+    currentIndex--;
 
-    if (currentImgIndex < 0){
-        currentImgIndex = lastIndex;
+    if (currentIndex < 0){
+        currentIndex = lastIndex;
     }
 
-    imgCarousel.src = imgList[currentImgIndex];
+    newImg.src = imgList[currentIndex];
 })
 
 btnDown.addEventListener("click", function(){
-    currentImgIndex++;
+    currentIndex++;
 
-    if (currentImgIndex > lastIndex){
-        currentImgIndex = 0;
+    if (currentIndex > lastIndex){
+        currentIndex = 0;
     }
 
-    imgCarousel.src = imgList[currentImgIndex];
+    newImg.src = imgList[currentIndex];
 })
